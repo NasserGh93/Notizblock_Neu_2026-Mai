60d7b3a75931b54ca1f8bbb88d85bffaecf36ab7
NasserGh93 <nasserghaibeh@googlemail.com>
2026-05-01 00:29:52 +0200

    updateFiles neu Folder

--- notes.js
+++ notes.js
@@ -1,46 +1,71 @@
-let notes = ['banana', 'rasen mähen'];
+let notesTitle = ['Ba', 'Aufgabe', 'Garten'];
+let notes = ['banana', 'Orange', 'rasenmähen'];
+
 let trashNotes = [];
+let trashNotesTitle = [];
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = "";
 
-    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        contentRef.innerHTML += getNoteTemplate(indexNote);
+    for (let i = 0; i < notes.length; i++) {
+        contentRef.innerHTML += getNoteTemplate(i);
     }
-}
-
-function getNoteTemplate(indexNote) {
-    return `<p>+ ${notes[indexNote]}</p> <button onclick="deleteNote(${indexNote})">X</button>`;
-}
-
-function addNote() {
-    let noteInputRef = document.getElementById('note_input');
-    let note_input = noteInputRef.value;
-
-    notes.push(note_input);
-
-    renderNotes();
-
-    noteInputRef.value = "";
-}
-
-function deleteNote(indexNote) {
-    trashNotes.push(notes[indexNote]);
-    notes.splice(indexNote, 1);
-    renderNotes();
-    renderTrashNotes();
 }
 
 function renderTrashNotes() {
     let trashContentRef = document.getElementById('trash_content');
     trashContentRef.innerHTML = "";
 
-    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
-        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    for (let i = 0; i < trashNotes.length; i++) {
+        trashContentRef.innerHTML += getTrashNoteTemplate(i);
     }
 }
 
-function getTrashNoteTemplate(indexNote) {
-    return `<p>+ ${trashNotes[indexNote]}</p>`;
+function getNoteTemplate(index) {
+    return `
+        <p>
+            >>> <b>${notesTitle[index]}</b> → ${notes[index]}
+            <button onclick="noteToTrash(${index})">x</button></p>`;
+}
+
+function getTrashNoteTemplate(index) {
+    return `
+        <p>
+            >>> <b>${trashNotesTitle[index]}</b> → ${trashNotes[index]}
+            <button onclick="deleteTrashNote(${index})">x</button>
+        </p>
+    `;
+}
+
+function addNote() {
+    let inputRef = document.getElementById('note_input');
+    let value = inputRef.value.trim();
+
+    if (value === "") return;
+
+    notes.push(value);
+    notesTitle.push("Neuer Titel");
+
+    inputRef.value = "";
+
+    renderNotes();
+}
+
+function noteToTrash(index) {
+    let deletedNote = notes.splice(index, 1)[0];
+    let deletedTitle = notesTitle.splice(index, 1)[0];
+
+    trashNotes.push(deletedNote);
+    trashNotesTitle.push(deletedTitle);
+
+    renderNotes();
+    renderTrashNotes();
+}
+
+function deleteTrashNote(index) {
+    trashNotes.splice(index, 1);
+    trashNotesTitle.splice(index, 1);
+
+    renderTrashNotes();
 }
